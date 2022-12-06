17a601969d887aec1f1bf42441e84b7ee983b749
KunHuo <610112077@qq.com>
2022-12-06 13:27:47 +0800

    Create dummy.R

---
+++ R/dummy.R
@@ -0,0 +1,85 @@
+dummy_exec <- function (x,
+                           method = c("treatment", "sum", "helmert", "poly", "full"),
+                           base = 1,
+                           levels = NULL,
+                           ...) {
+
+  if (is.null(levels))
+    x <- factor(x)
+  else
+    x <- factor(x, levels = levels)
+  if (!is.numeric(base))
+    base <- match(base, levels(x))
+  method <- match.arg(arg = method,
+                      choices = c("treatment", "sum", "helmert", "poly", "full"))
+  switch(
+    method,
+    treatment = {
+      res <- stats::contr.treatment(n = nlevels(x), base = base)[x,, drop = FALSE]
+    },
+    sum = {
+      res <- stats::contr.sum(n = nlevels(x))[x, , drop = FALSE]
+    },
+    helmert = {
+      res <- stats::contr.helmert(n = nlevels(x))[x, , drop = FALSE]
+    },
+    poly = {
+      res <- stats::contr.poly(n = nlevels(x))[x, , drop = FALSE]
+    },
+    full = {
+      res <- diag(nlevels(x))[x, , drop = FALSE]
+    }
+  )
+  res <- as.matrix(res)
+  if (method == "full") {
+    dimnames(res) <-
+      list(if (is.null(names(x)))
+        1L:length(x)
+        else
+          names(x),
+        levels(x))
+    attr(res, "base") <- NA
+  }
+  else {
+    dimnames(res) <-
+      list(if (is.null(names(x)))
+        1L:length(x)
+        else
+          names(x),
+        levels(x)[-base])
+    attr(res, "base") <- levels(x)[base]
+  }
+  return(res)
+}
+
+
+dummy <- function(x,
+                  method = c("treatment", "sum", "helmert", "poly", "full"),
+                  base = 1,
+                  levels = NULL,
+                  varnames = NULL,
+                  keep = FALSE, sep = "_", ...) {
+
+  method <- match.arg(method)
+
+  if(is.null(varnames)){
+    varnames <- names(x)[sapply(x, function(i) is.factor(i) | is.character(i))]
+  }else{
+    check_name(x, varnames)
+  }
+
+  if(length(varnames) == 0L){
+    return(x)
+  }
+
+  for(i in seq_along(varnames)){
+    res <- dummy_exec(x[[varnames[i]]], method = method, base = base)
+    res <- as.data.frame(res)
+    names(res) <- paste(varnames[i], names(res), sep = sep)
+    x <- append2(x, res, after = varnames[i])
+    if(!keep){
+      x[which(names(x) == varnames[i])] <- NULL
+    }
+  }
+  x
+}
